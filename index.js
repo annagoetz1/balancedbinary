0dc122cb1ac8d076c1327908a4019f3e7824860d
annagoetz1 <annalgoetz1@gmail.com>
2025-01-05 12:35:14 -0500

    Add initial node class with instance

---
+++ index.js
@@ -0,0 +1,10 @@
+// JavaScript program to convert sorted
+// array to BST.
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
